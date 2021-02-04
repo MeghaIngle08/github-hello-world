71a33bdfad44c52510dc21c133c909fc73592eac
MeghaIngle08 <78537764+MeghaIngle08@users.noreply.github.com>
2021-02-04 16:32:10 +0530

    Initial commit of math.py

---
+++ math.py
@@ -0,0 +1,13 @@
+#Addition Functionality
+def add(x,y):
+    pass
+#Substraction Functionality
+def subtract(x,y):
+    pass
+    
+#Multipication Functionality
+def multiply(x,y):
+    pass
+    
+def divide(x,y):
+    pass
